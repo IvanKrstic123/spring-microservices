ab254b3928852a533eb40950b524e42ca6e16b14
Ivan Krstic <krstic8687@yahoo.com>
2021-10-13 22:50:32 +0200

    Setting up Currency Exchange Microservice with JPA Repository

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -0,0 +1,42 @@
+package com.microservices.currencyexchangeservice.controllers;
+
+import com.microservices.currencyexchangeservice.model.CurrencyExchange;
+import com.microservices.currencyexchangeservice.repositories.CurrencyExchangeRepository;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+
+    private final CurrencyExchangeRepository currencyExchangeRepository;
+
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository) {
+        this.environment = environment;
+        this.currencyExchangeRepository = currencyExchangeRepository;
+    }
+
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
+                                                  @PathVariable String to) {
+
+        String port = environment.getProperty("local.server.port");
+
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to Find Data from " + from + "  to " + to);
+        }
+
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+    }
+
+}
