66f829fe7de3fa5f271c2e19b5978e214d36b095
Ivan Krstic <krstic8687@yahoo.com>
2021-10-17 14:38:02 +0200

    Implementing Spring Cloud Gateway Global Logging Filter

---
+++ api-gateway/src/main/java/com/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,46 @@
+package com.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+
+    // customizing routes we will want to use
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                            .filters(f -> f
+                                    .addRequestHeader("MyHeader", "MyURI")
+                                    .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+
+                .route(p -> p.path("/currency-exchange/**") // bilo sta **
+                        .uri("lb://currency-exchange"))  // load balance
+
+                .route(p -> p.path("/currency-conversion/**") // bilo sta **
+                        .uri("lb://currency-conversion"))  // load balance
+
+                .route(p -> p.path("/currency-conversion-feign/**") // bilo sta **
+                        .uri("lb://currency-conversion"))  // load balance
+
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"
+                        ))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
